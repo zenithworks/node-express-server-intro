111bd494132cf8b469147fe4b59ca68466f96c75
raiyan <raiyan@raiyan.com>
2019-09-02 12:02:08 +0530

    added dynamic get

--- server.js
+++ server.js
@@ -2,19 +2,8 @@ const express = require('express');
 const app = express();
 
 
-const mockUserData=[
-    {name:'Mark'},
-    {name:'Jill'}
-    ]
-    app.get('/users', function(req,res){
-         res.json({
-              success: true,
-              message: 'successfully got users. Nice!',
-              users: mockUserData
-         })
-    })
 
+    app.get('/users/:id',function(req,res){
+        console.log(req.params.id)
+        })
 
-app.listen(8000,function(){
-    console.log("Ray's server is running...")
-    })
