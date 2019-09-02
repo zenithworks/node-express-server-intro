b7999cf3c1fc8a306d233c9f5c2536477f97a62a
raiyan <raiyan@raiyan.com>
2019-09-02 11:46:53 +0530

    added first get route

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 
 
+const mockUserData=[
+    {name:'Mark'},
+    {name:'Jill'}
+    ]
+    app.get('/users', function(req,res){
+         res.json({
+              success: true,
+              message: 'successfully got users. Nice!',
+              users: mockUserData
+         })
+    })
+
 
 app.listen(8000,function(){
     console.log("Ray's server is running...")
