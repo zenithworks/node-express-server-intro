f7e32d5c8f2944aca2fce0ee1a0e4adb3d37387d
raiyan <raiyan@raiyan.com>
2019-09-02 12:38:38 +0530

    added post route

--- server.js
+++ server.js
@@ -2,8 +2,26 @@ const express = require('express');
 const app = express();
 
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
-    app.get('/users/:id',function(req,res){
-        console.log(req.params.id)
-        })
+app.post('/login',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
 
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username===mockUsername && password===mockPassword){
+         res.json({
+              success: true,
+              message: 'password and username match!',
+              token: 'encrypted token goes here'
+         })
+    } else {
+         res.json({
+              success: false,
+              message: 'password and username do not match'
+         })
+    }
+})
